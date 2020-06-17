4d648c9270b36a2adcc3ccd49d69d29f900c3584
Dhaval-Bheda <dhavalbhedadb@gmail.com>
2020-06-17 22:47:25 +0530

    Updates

---
+++ precache-manifest.ed2a3c296a0d6672fa53c9a57343f641.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea5721b5f65920fd42d5bbd85d1c29a",
+    "url": "/quize-app/index.html"
+  },
+  {
+    "revision": "aba2e5c9dead655463b7",
+    "url": "/quize-app/static/css/2.cb7199dc.chunk.css"
+  },
+  {
+    "revision": "c04e7c5cadb6f5550cfb",
+    "url": "/quize-app/static/css/main.7f30e631.chunk.css"
+  },
+  {
+    "revision": "aba2e5c9dead655463b7",
+    "url": "/quize-app/static/js/2.fb7a141a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/quize-app/static/js/2.fb7a141a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04e7c5cadb6f5550cfb",
+    "url": "/quize-app/static/js/main.0448a7dc.chunk.js"
+  },
+  {
+    "revision": "87fc8b110994d4796fc6",
+    "url": "/quize-app/static/js/runtime-main.bd185746.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/quize-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/quize-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/quize-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/quize-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/quize-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
